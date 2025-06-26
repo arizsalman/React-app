22e1c6eb59c3aaadb2097de2ea331bd3e12164c9
arizsalman <muhammadarizsalman@gmail.com>
2025-06-27 00:22:29 +0500

    Todo
    Add list

---
+++ src/assets/compounent/TodoItems.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const TodoItems = () => {
+  let TodoItems= [
+
+    {
+      name:"Buy Milk",
+      dueDate:"27/9/2025"
+    }
+    , {
+      name:"Buy Milk",
+      dueDate:"27/9/2025"
+    },
+     {
+      name:"Buy Milk",
+      dueDate:"27/9/2025"
+    }
+  ]
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TodoItems
