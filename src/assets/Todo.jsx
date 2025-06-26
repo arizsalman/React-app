d46a5d990ab438b4e4e9922d9ce72437edff0588
arizsalman <muhammadarizsalman@gmail.com>
2025-06-27 00:37:58 +0500

    done list

--- src/assets/Todo.jsx
+++ src/assets/Todo.jsx
@@ -4,16 +4,23 @@ import TodoItem from './compounent/TodoItem'
 import TodoType1 from './compounent/TodoType1'
 
 
+
 const Todo = () => {
+  const TodoItems = [
+    { name: "Buy Milk", dueDate: "27/9/2025" },
+    { name: "Add bill", dueDate: "28/9/2025" },
+    { name: "Done Homework", dueDate: "12/9/2025" },
+  ];
   return (
    <center className='todo-cantainer'>
     <TodoApp/>
     <TodoItem/>
     <div className='item-cantainer'>
     
-    <TodoType1 todoName="27/9/2025" todoDate="Buy Milk"> </TodoType1>
-    <TodoType1 todoName="21/11/2025" todoDate="Add new bike"> </TodoType1>
-    <TodoType1 todoName="12/10/2025" todoDate="Add Bill"> </TodoType1>
+    {TodoItems.map(item => (
+        <TodoType1 key={item.name} todoName={item.name} todoDate={item.dueDate} />
+      ))}
+   
     
     </div>
     
--- src/assets/compounent/TodoType1.jsx
+++ src/assets/compounent/TodoType1.jsx
@@ -7,6 +7,7 @@ const TodoType1 = ({todoName,todoDate}) => {
         <div class="row">
     <div class="col-6" > {todoName}</div>
     <div class="col-4"> {todoDate} </div>
+    
     <div class="col-2"><button type="button" class="btn btn-danger">delete</button>
     </div>
   </div>
