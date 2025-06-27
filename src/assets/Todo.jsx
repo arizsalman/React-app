7f9f8517a8f377417ecba94573535599b1d15bfa
arizsalman <muhammadarizsalman@gmail.com>
2025-06-28 01:53:05 +0500

    Delete mehtod tommorw

--- src/assets/Todo.jsx
+++ src/assets/Todo.jsx
@@ -2,16 +2,18 @@ import React, { useState } from 'react'
 import TodoApp from './compounent/TodoApp'
 import TodoItem from './compounent/TodoItem'
 import TodoType1 from './compounent/TodoType1'
+import WellcomeMasege from './compounent/WellcomeMasege'
 
 
 
 const Todo = () => {
-  const initialTodoItems = [
-    { name: "Buy Milk", dueDate: "27/9/2025" },
-    { name: "Add bill", dueDate: "28/9/2025" },
-    { name: "Done Homework", dueDate: "12/9/2025" },
-  ];
-  const [todoItems,setTodoItems] =useState(initialTodoItems)
+  // const initialTodoItems = [
+  //   { name: "Buy Milk", dueDate: "27/9/2025" },
+  //   { name: "Add bill", dueDate: "28/9/2025" },
+  //   { name: "Done Homework", dueDate: "12/9/2025" },
+  // ];
+  // const [todoItems,setTodoItems] =useState(initialTodoItems)
+  const [todoItems,setTodoItems] =useState([])
   const handleNewItem = (todoName, todoDate )=>{
     console.log(`New Item Add : ${todoName}`,`Date:${todoDate}`);  
   
@@ -21,11 +23,14 @@ const Todo = () => {
  setTodoItems(newTodoItem) 
 }
 
+const handleDeleteItem=()=>{}
+
 
   return (
    <center className='todo-cantainer'>
     <TodoApp/>
     <TodoItem onNewItem={handleNewItem} />
+    {todoItems.length === 0 &&<WellcomeMasege></WellcomeMasege>}
     <div className='item-cantainer'>
     
     {todoItems.map(item => (
